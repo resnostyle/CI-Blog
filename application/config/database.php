a4c2940c68495a1bf34ef088c0af924b2d9ab97c
BryanWp <bwp.pearson@gmail.com>
2011-12-14 20:57:30 -0500

    quick fix

--- application/config/database.php
+++ application/config/database.php
@@ -43,9 +43,9 @@ $active_group = "default";
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "root";
-$db['default']['password'] = "1600";
-$db['default']['database'] = "blog";
+$db['default']['username'] = "";
+$db['default']['password'] = "";
+$db['default']['database'] = "";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
